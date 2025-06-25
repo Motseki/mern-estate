1b5137bb90facf8fcda5a98a1f39225013c5ea05
Lebohang Motseki <lbmotseki@gmail.com>
2025-06-25 13:23:58 +0200

    Created header component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
@@ -8,3 +9,5 @@ export default {
   },
   plugins: [],
 }
+
+
