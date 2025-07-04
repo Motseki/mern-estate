8341df7c7ae1553505d308125ec4af2912487195
Lebohang Motseki <lbmotseki@gmail.com>
2025-07-04 20:43:41 +0200

    Fixed the packages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api':{
+      '/api': {
         target: 'http://localhost:3000',
         secure: false,
       },
